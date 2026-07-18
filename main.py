1ca269b192b6dde231db3def5c3b6e3af2bae2d0
pythonpetar-cloud <pythonpetar@gmail.com>
2026-07-18 18:18:15 +0200

    Organize downloads into per-item folders

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from scraper import ParliamentScraper
+from scraper import ParliamentScraper, rename_all, DOWNLOAD_PATH
 from time import sleep
 from random import uniform
-from scraper import rename_all, DOWNLOAD_PATH
 
 bot = ParliamentScraper()
 
-bot.login()
-sleep(uniform(0.5, 1.5))
-
 try:
+    bot.login()
+
+    sleep(
+        uniform(0.5, 1.5)
+    )
     bot.download_docs()
+
 finally:
     bot.logout()
     bot.driver.quit()
